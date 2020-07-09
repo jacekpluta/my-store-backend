f8cd24b40493445e3e05182ab400aeba851326e3
Jacek Pluta <j.pluta@student.po.edu.pl>
2020-07-09 19:28:50 +0200

    delete items feature added

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -11,6 +11,61 @@ const Mutation = {
     );
     return item;
   },
+
+  updateItem(parrent, args, ctx, info) {
+    const updates = { ...args };
+
+    delete updates.id;
+
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+
+  async deleteItem(parrent, args, ctx, info) {
+    const where = { id: args.id };
+
+    const item = await ctx.db.query.item({ where }, `{id, title}`);
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signUp(parrent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    const password = await bcrypt.hash(args.password, 10);
+
+    const permission = {
+      set: "USER",
+    };
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          //name, email, password
+          ...args,
+          password: password,
+          permissions: permission,
+        },
+      },
+      info
+    );
+
+    //JWT TOKEN
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    console.log("coooooooooooooookie", ctx.response.cookie);
+
+    return user;
+  },
 };
 
 module.exports = Mutation;
