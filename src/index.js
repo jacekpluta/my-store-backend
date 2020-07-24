cfb7d4e4bdff4892c3208e510ae8b7604ae5df65
Jacek Pluta <j.pluta@student.po.edu.pl>
2020-07-24 17:13:09 +0200

    gitignore updated

--- src/index.js
+++ src/index.js
@@ -44,12 +44,12 @@ server.express.use(async (req, res, next) => {
 });
 
 server.start(
-  // {
-  //   cors: {
-  //     credentials: true,
-  //     origin: process.env.FRONTEND_URL,
-  //   },
-  // },
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
   (deets) => {
     console.log(`server running on ${deets.port}`);
   }
