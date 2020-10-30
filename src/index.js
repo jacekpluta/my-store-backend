a07e7e006e874a3b8c63c82ad0c8e72e2d846654
Jacek <jacagaca1@o2.pl>
2020-10-30 13:51:59 +0100

    checking cookies

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ server.get("/home", (req, res) => {
   return res.send("Hello");
 });
 
+server.applyMiddleware({
+  app,
+  path: "/",
+  cors: false, // disables the apollo-server-express cors to allow the cors middleware use
+});
+
 //express middleware to handle cookies JWT
 //parses Cookie header and populate req.cookies
 server.express.use(cookieParser());
